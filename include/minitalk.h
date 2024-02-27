300f24fda547c44e1b25dbf99803e76f1551798a
Dorde Dordevic <dodordev@c0c1c2.42berlin.de>
2024-02-27 17:09:22 +0100

    start

---
+++ include/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dodordev <dodordev@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 12:40:07 by dodordev          #+#    #+#             */
+/*   Updated: 2024/02/27 16:53:05 by dodordev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+
+
+
+#endif
